a96c071a2153e45a33f6ed9cda3cbe64c1e0df3f
soumia el mousstari <elmousstarisoumia@gmail.com>
2024-06-04 04:24:49 -0700

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-    if response.status_code >= 300:
+    if response.status_code != 200:
         return 0
 
     return response.json().get("data").get("subscribers")
