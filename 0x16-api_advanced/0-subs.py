e65caaadbd0ed1cab4133268119d3d2777fea8db
soumia el mousstari <elmousstarisoumia@gmail.com>
2024-06-04 04:19:11 -0700

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,19 +3,19 @@
 Script that queries subscribers on a given Reddit subreddit.
 """
 
+import requests
+
 
 def number_of_subscribers(subreddit):
     """Write a function that queries the Reddit API an
     returns the number of subscribers (not active users,
     total subscribers) for a given subreddit. If an
     invalid subreddit is given, the function should return 0."""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+    response = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-    if sub_info.status_code >= 300:
+    if response.status_code >= 300:
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    return response.json().get("data").get("subscribers")
